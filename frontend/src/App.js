71bb54cfa6420e84060e85f204a33df2eea79071
joshuaconstine <joshua.constine97@gmail.com>
2024-09-17 19:17:35 -0700

    moved components inside /src

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Appointment from '../components/appointment';
+
+import Appointment from './components/Appointment';
 
 function App() {
 
---
+++ frontend/src/components/Appointment.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Appointment = () => {
+
+    //get the list of appointments from api
+    
+    return (
+        <div>
+            <h1>Appointment</h1>
+        </div>
+    );
+}
+
+export default Appointment;
