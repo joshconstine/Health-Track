7af551ba1ba68e1e41e41ef4a5d0a7a41d4bcb01
Ruben <rguillen1381@sdsu.edu>
2024-09-26 17:10:36 -0700

    edited these two files will continue to work on it

--- frontend/src/routes/Appointment.jsx
+++ frontend/src/routes/Appointment.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React , {useState, useEffect} from "react";
 
 const Appointment = () => {
 
+    const [appointments, setAppointments] = useState([]);
+
+    const fetchAppointments = async () => {
+        const response = await fetch('http://localhost:4000/appointments');
+        const data = await response.json();
+        setAppointments(data);
+    }
+
+    useEffect(() => {
+        fetchAppointments();
+    }, []);
+
     return (
         <div>
-            <h1>Appointment</h1>
+            <h1>Appointments</h1>
+            <ul>
+                {appointments.map((appointment) => (
+                    <div key={appointment.id}>  {/* Ensure you're using a unique key for each item */}
+                        <h3>Patient ID: {appointment.patient_id}</h3>
+                            <p>Date: {appointment.date}</p>
+                            <p>Time: {appointment.time}</p>
+                            <p>Reason: {appointment.reason}</p>
+                            <p>Created At: {appointment.created_at}</p>
+                    </div>
+                ))}
+            </ul>
         </div>
     );
 }
