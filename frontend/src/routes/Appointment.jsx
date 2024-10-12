2376b02d999425fe7597d9db78965090df852f72
Stephen <sparkerr.96@gmail.com>
2024-10-12 14:56:40 -0700

    added appointment lab orders and billable services to front end

--- frontend/src/routes/Appointment.jsx
+++ frontend/src/routes/Appointment.jsx
@@ -1,36 +1,72 @@
-import React from "react";
+import React from 'react';
 
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-const Appointment = () =>{
-    const params = useParams();
-    const id = params.id;
+const Appointment = () => {
+  const params = useParams();
+  const id = params.id;
 
-    const [appointment, setAppointment] = useState({});
+  const [appointment, setAppointment] = useState({});
+  const [labOrders, setOrders] = useState([]);
+  const [billableServices, setSerives] = useState([]);
 
-    const fetchAppointment = async (id) => {
-        const response = await fetch(`http://localhost:4000/appointments/${id}`);
-        const data = await response.json();
-        setAppointment(data);
-    }
+  const fetchAppointment = async (id) => {
+    const response = await fetch(`http://localhost:4000/appointments/${id}`);
+    const data = await response.json();
+    setAppointment(data);
+    const labOrderResponse = await fetch(
+      `http://localhost:4000/appointments/${id}/labOrders`
+    );
+    const labOrderData = await labOrderResponse.json();
+    setOrders(labOrderData);
+    const billableServicesResponse = await fetch(
+      `http://localhost:4000/appointments/${id}/labOrders`
+    );
+    const billableServicesData = await billableServicesResponse.json();
+    setSerives(billableServicesData);
+  };
 
-    useEffect(() => {
-        fetchAppointment(id);
-    }, []);
-    return(
-        <div>
-            <h1>Appointment</h1>
-            <p>{appointment.first_name}</p>
-            <p>{appointment.id}</p>
-            <p>{appointment.last_name}</p>
-            <p>{appointment.name}</p>
-            <p>{appointment.pager_number}</p>
-            <p>{appointment.phone_number}</p>
-            <p>{appointment.start_time}</p>
-            <p>{appointment.end_time}</p>
+  useEffect(() => {
+    fetchAppointment(id);
+  }, []);
+  return (
+    <div>
+      <h1>Appointment</h1>
+      <p>{appointment.first_name}</p>
+      <p>{appointment.id}</p>
+      <p>{appointment.last_name}</p>
+      <p>{appointment.name}</p>
+      <p>{appointment.pager_number}</p>
+      <p>{appointment.phone_number}</p>
+      <p>{appointment.start_time}</p>
+      <p>{appointment.end_time}</p>
 
-        </div>
-    )
-}
+      <ul>
+        {labOrders.map((order) => (
+          <div key={order.id}>
+            {' '}
+            {/* Ensure you're using a unique key for each item */}
+            <h3>Lab Name: {order.name}</h3>
+            <p>Lower Bound: {order.lower_bound}</p>
+            <p>Upper Bound: {order.upper_bound}</p>
+            <p>Measured Value: {order.measured_value}</p>
+            <p>Lab Technician: {order.lab_technician_name}</p>
+          </div>
+        ))}
+      </ul>
 
-export default Appointment; 
+      <ul>
+        {billableServices.map((service) => (
+          <div key={service.invoice_id}>
+            {' '}
+            {/* Ensure you're using a unique key for each item */}
+            <h3>Service Name: {service.description}</h3>
+            <p>Service Cost: {service.cost}</p>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Appointment;
