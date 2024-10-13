68a2ee10d90e9f4d450da46eb2d6b2f9aaaa9b7e
joshuaconstine <joshua.constine97@gmail.com>
2024-10-13 12:37:47 -0700

    updated insurance carrier to use table.

--- frontend/src/routes/InsuranceCarriers.jsx
+++ frontend/src/routes/InsuranceCarriers.jsx
@@ -14,21 +14,34 @@ const InsuranceCarriers = () => {
     useEffect(() => {
         fetchInsuranceCarriers();
     }, []);
-
+   
     return (
         <div>
             <h1>InsuranceCarriers</h1>
-            <ul>
-                {insuranceCarriers.map((insurance) => {
-                    return  <div key={insurance.id}>
-                        <Link
-                            to={`/insuranceCarriers/${insurance.id}`}
-                        >{insurance.name} ID: {insurance.id} </Link>
-                        <p>{insurance.address}</p>
-                        <p>{insurance.status_name}</p>
-                    </div>
-                })}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>InsuranceCarrier Id</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {insuranceCarriers.map((insurance) => (
+                        <tr key={insurance.id}>
+                            <td>
+                                <Link to={`/insuranceCarriers/${insurance.id}`}>
+                                    {insurance.id}
+                                </Link>
+                            </td>
+                            <td>{insurance.name}</td>
+                            <td>{insurance.address}</td>
+                            <td>{insurance.status_name}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+            </table>
         </div>
     );
 }
