d68232a76805dcac80a72d3502f2dca89dbbf5f7
joshuaconstine <joshua.constine97@gmail.com>
2024-10-16 12:54:57 -0700

    fetching laborders in component

--- frontend/src/routes/LabOrder.jsx
+++ frontend/src/routes/LabOrder.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const LabOrder = () => {
+    const [labOrders, setLabOrders] = React.useState([]);
+    // labOrders =[{
+    //     id:1 
+    // }]
+    const fetchLabOrders = async () => {
+        const response = await fetch("http://localhost:4000/labOrders");
+        const data = await response.json();
+        setLabOrders(data);
+    }
 
+    React.useEffect(() => {
+        fetchLabOrders();
+    }, []);
+        
     return (
         <div>
             <h1>Lab order tracker</h1>
