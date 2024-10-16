5ca0d8000a59e7c250b9156cd38eef8c69b67957
joshuaconstine <joshua.constine97@gmail.com>
2024-10-16 14:25:28 -0700

    added filter on lab order for practitiner id

--- backend/index.js
+++ backend/index.js
@@ -82,6 +82,9 @@ app.get('/patients', (req, res) => {
 app.get('/labOrders', (req, res) => {
   // Define the SQL query that selects first and last names from the employees table
   // and gets their practitioner type from the practitioner_types table.
+
+  const selectedPractitionerId = req.query.practitioner_id
+
   const query = `
 SELECT l.ID
 , ltt.name
@@ -99,11 +102,28 @@ join patients p on  p.id = l.patient_id
 join practitioners pr on pr.id = l.ordered_by_physician_id
 join employees e on pr.employee_id = e.employee_id;`
 
+const filterdQuery = `
+  SELECT l.ID
+  , ltt.name
+  ,l.patient_id
+  ,l.ordered_by_physician_id
+  ,l.appointment_id
+  ,l.lab_technician_id
+  ,l.measured_value
+  ,l.date_taken
+  ,CONCAT(p.first_name, ' ', p.last_name) AS patient_name
+  ,CONCAT(e.first_name, ' ',e.last_name) as practitioner_name
+  FROM lab_orders l
+  join health.lab_test_types ltt on ltt.id = l.lab_test_type_id
+  join patients p on  p.id = l.patient_id
+  join practitioners pr on pr.id = l.ordered_by_physician_id
+  join employees e on pr.employee_id = e.employee_id
+  where l.ordered_by_physician_id = ${selectedPractitionerId}`
 
 
   // Try to run the query on the database
   try {
-    connection.query(query, (err, rows) => {
+    connection.query(selectedPractitionerId ? filterdQuery :query, (err, rows) => {
       // Run the SQL query
       if (err) throw err; // If there is an error, throw it
 
@@ -389,16 +409,15 @@ app.get('/practitioners', (req, res) => {
       practitioner_types pt   // Join 'practitioner_types' to get the type of practitioner (like doctor, nurse)
       ON p.practitioner_type_id = pt.id  // Match the practitioner type using practitioner_type_id
   `;
-
   // Try to run the query on the database
   try {
-    connection.query(query, (err, rows) => {
+    connection.query(  query, (err, rows) => {
       // Run the SQL query
       if (err) throw err; // If there is an error, throw it
 
       res.json(rows); // Send the result (rows) back to the client (your React app) in JSON format
     });
-  } catch (error) {
+    } catch (error) {
     // If there's an error in running the query or connecting to the database,
     // log the error and send a 500 status (server error) to the client.
     console.log(error);
