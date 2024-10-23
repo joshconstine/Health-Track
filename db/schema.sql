fad862e4ea9c782f58ad6a269caa6439bf5b3393
joshuaconstine <joshua.constine97@gmail.com>
2024-10-22 18:47:32 -0700

    added gender to patients

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE patients (
     phone_number VARCHAR(15) NOT NULL,
     email VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL,
-    gender VARCHAR(20) NOT NULL,
+    gender VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE medical_encounters (
