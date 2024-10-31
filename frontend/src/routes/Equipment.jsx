311b880e6bb6756a6ba837982167a372a6a6cf76
TaliaG16 <taliagoody@gmail.com>
2024-10-30 17:59:08 -0700

    Added table of equipment with link to single equipment page

--- frontend/src/routes/Equipment.jsx
+++ frontend/src/routes/Equipment.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React , {useState, useEffect} from "react";
 
-
+import { Link } from 'react-router-dom';
 const Equipment = () => {
-    console.log('hi talia')
+
+    const [equipment, setEquipment] = useState([]);
+
+    const fetchEquipment = async () => {
+        const response = await fetch('http://localhost:4000/equipment');
+        const data = await response.json();
+        setEquipment(data);
+    }
+
+    useEffect(() => {
+        fetchEquipment();
+    }, []);
+  
     return (
         <div>
-        Equipment hello
-    </div>
-    )
+            <h1>Equipment Inventory</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Equipment Id</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                        <th>Owned or Lease</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {equipment.map((equipment) => (
+                        <tr key={equipment.id}>
+                            <td><Link to={`/equipment/${equipment.id}`}>
+                                {equipment.id}
+                            </Link></td>
+                            <td>{equipment.name}</td>
+                            <td>{equipment.description}</td>
+                            <td>{equipment.owned_leased}</td>
+                            <td>{equipment.status}</td>
+                        </tr>
+                    ))} 
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Equipment;
