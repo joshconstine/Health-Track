df3a918efb8d3a3b6688ebd14d5372f577113ad5
Stephen <sparkerr.96@gmail.com>
2024-10-30 19:52:40 -0700

    added styling to singleEquip view and removed hardcoded commented out data

--- frontend/src/routes/SingleEquipment.jsx
+++ frontend/src/routes/SingleEquipment.jsx
@@ -1,35 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './SingleEquipment.css';
 
 const SingleEquipment = () => {
   const params = useParams();
   const id = params.id;
-  //   {
-  //     "id": 1,
-  //     "name": "xray_machine",
-  //     "description": "Machine for taking X-rays",
-  //     "owned_leased": "Owned",
-  //     "status": "under maintenance",
-  //     "equipment_id": 1,
-  //     "lease_start": null,
-  //     "lease_end": null,
-  //     "leased_from": null,
-  //     "lease_description": null,
-  //     "date_purchased": "2020-01-01T08:00:00.000Z",
-  //     "warranty_expiration": "2025-01-01T08:00:00.000Z",
-  //     "warranty_description": "5-year warranty on parts"
-  // }
   const [equipmentData, setEquipmentData] = useState({});
-  //   [
-  //     {
-  //         "id": 1,
-  //         "name": "calibration_issue",
-  //         "description": "Requires recalibration",
-  //         "resolution": "Repaired mechanical failure in X-Ray Machine"
-  //     }
-  // ]
   const [equipmentMaintenance, setEquipmentMaintenance] = useState([]);
   const fetchEquipment = async (id) => {
     const response = await fetch(`http://localhost:4000/equipment/${id}`);
@@ -44,58 +22,57 @@ const SingleEquipment = () => {
   useEffect(() => {
     fetchEquipment(id);
   }, []);
+
+  console.log(equipmentMaintenance.length);
   return (
-    <div>
-      <h1>Equipment {equipmentData.id}</h1>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-          marginBottom: "20px",
-        }}
-      >
-        <span>name: {equipmentData.name}</span>
-        <span>description: {equipmentData.description}</span>
-        <span>owned_leased: {equipmentData.owned_leased}</span>
-        {equipmentData.owned_leased === "Owned" ? (
+    <div className='EquipmentContainer'>
+      <div className='EquipmentBody'>
+        <h1>Equipment {equipmentData.id}</h1>
+        <p>name: {equipmentData.name}</p>
+        <p>description: {equipmentData.description}</p>
+        <p>owned_leased: {equipmentData.owned_leased}</p>
+        {equipmentData.owned_leased === 'Owned' ? (
           <>
-            <span>date_purchased: {equipmentData.date_purchased}</span>
-            <span>
-              warranty_expiration: {equipmentData.warranty_expiration}
-            </span>
-            <span>
-              warranty_description: {equipmentData.warranty_description}
-            </span>
+            <p>date_purchased: {equipmentData.date_purchased}</p>
+            <p>warranty_expiration: {equipmentData.warranty_expiration}</p>
+            <p>warranty_description: {equipmentData.warranty_description}</p>
           </>
         ) : (
           <>
-            <span>lease_start: {equipmentData.lease_start}</span>
-            <span>lease_end: {equipmentData.lease_end}</span>
-            <span>leased_from: {equipmentData.leased_from}</span>
-            <span>lease_description: {equipmentData.lease_description}</span>
+            <p>lease_start: {equipmentData.lease_start}</p>
+            <p>lease_end: {equipmentData.lease_end}</p>
+            <p>leased_from: {equipmentData.leased_from}</p>
+            <p>lease_description: {equipmentData.lease_description}</p>
           </>
         )}
 
-        <span>status: {equipmentData.status}</span>
+        <p>status: {equipmentData.status}</p>
       </div>
-      <ul>
-        <h2>Maintenance</h2>
-        {equipmentMaintenance.map((maintenance) => (
-          <li
-            key={maintenance.id}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "10px",
-            }}
-          >
-            <span>name: {maintenance.name}</span>
-            <span>description: {maintenance.description}</span>
-            <span>resolution: {maintenance.resolution}</span>
-          </li>
-        ))}
-      </ul>
+      {equipmentMaintenance.length > 0 ? (
+        <div>
+          <h2 align='center'>Maintenance</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Issue</th>
+                <th>Description</th>
+                <th>Resolution</th>
+              </tr>
+            </thead>
+            <tbody>
+              {equipmentMaintenance.map((maintenance) => (
+                <tr key={maintenance.id}>
+                  <td>{maintenance.name}</td>
+                  <td>{maintenance.description}</td>
+                  <td>{maintenance.resolution}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
